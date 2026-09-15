392ebca6f76c857b178598476b8d563deaec126a
YalalovNR <amina@DESKTOP-E93E8MP>
2026-09-15 16:50:42 +0300

    Task3

---
+++ Tyuiu.YalalovNR.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.YalalovNR.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.YalalovNR.Sprint0.Task3.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckedValid()
+        {
+
+
+            Assert.Equal(10, DataService.Sum(5, 5));
+        }
+
+
+
+    }
+}
+
+
+    
